82a3bb07ef879a4ca0653e4a23fdd248f89479d1
hieu22012003 <101488865+hieu22012003@users.noreply.github.com>
2022-03-13 21:32:28 +0700

    btvn1
    name

---
+++ bai tap 11.3.2022/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int a;
+  int b;
+  int c;
+  
+  printf("nhap so bat ky \n");
+  scanf("%d", &a);
+  
+  printf("nhap so bat ky \n");
+  scanf("%d", &b);
+  
+  c=a+b;
+  printf("tong so c la %d  \n", c);
+  system("PAUSE");	
+  return 0;
+}
---
+++ bai tap 11.3.2022/main2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  char fullname[100];
+  printf("hi...");
+  gets(fullname);
+  printf("wellcome %s to quang binh \n", fullname);
+  return 0;
+}
